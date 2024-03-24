570d46ef7935a03c7622be1e22a0799b993984c0
singularitti <singularitti@outlook.com>
2024-03-24 03:24:47 -0400

    Add `setup_hamiltonian` in benchmark/benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,7 +1,21 @@
 using FermiDiracOperatorExpansion
 using BenchmarkTools
+using LinearAlgebra: Diagonal, Symmetric
 
 SUITE = BenchmarkGroup()
 SUITE["rand"] = @benchmarkable rand(10)
 
 # Write your benchmarks here.
+
+const kBT = 0.25
+const μ = 0.1
+
+function setup_hamiltonian(N, a=0.01)
+    𝐇 = Diagonal(10 * rand(N))
+    foreach(1:N) do i
+        foreach(1:N) do j
+            𝐇[i, j] = exp(-a * (i - j)^2)  # Mimic a non-metallic system or a metallic system at ﬁnite temperature
+        end
+    end
+    return Symmetric(𝐇)
+end
