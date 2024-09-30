78f9064b8c6f6ce6855fac60037baf3ca92a932e
singularitti <singularitti@outlook.com>
2024-09-30 03:14:44 -0600

    Add `estimate_mu`

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,5 +1,6 @@
 using FermiDiracOperatorExpansion
 using BenchmarkTools
+using GershgorinDiscs
 using LinearAlgebra: Symmetric, diagm, tr
 
 SUITE = BenchmarkGroup()
@@ -16,7 +17,12 @@ function setup_hamiltonian(N, a=0.01)
     return Symmetric(𝐇)
 end
 
-β = 40
+function estimate_mu(β, N, λₘᵢₙ, λₘₐₓ)
+    η = exp(β * N)
+    return inv(β) * log((η - 1) / (exp(-β * λₘᵢₙ) - η * exp(-β * λₘₐₓ)))
+end
+
+β = 4
 μ = 0.1
 𝐇 = setup_hamiltonian(1000)
 
@@ -28,9 +34,11 @@ order_α_exact = get_order(α_exact, β)
 
 dm = density_matrix(𝐇, μ, α; order)
 N = tr(dm)
+mu = estimate_mu(β, N, eigvals_extrema(𝐇)...)
 
 dm_α_exact = density_matrix(𝐇, μ, α_exact; order=order_α_exact)
 N_α_exact = tr(dm_α_exact)
 
 dm_exact = fermi_dirac(𝐇, μ, β)
 N_exact = tr(dm_exact)
+mu_exact = estimate_mu(β, N_exact, extrema(eigvals(𝐇))...)
