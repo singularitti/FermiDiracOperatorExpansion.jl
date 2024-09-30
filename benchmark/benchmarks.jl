554bc4b3970a7e85052edc4a6f6aa0ba9b09e97f
singularitti <singularitti@outlook.com>
2024-09-29 22:20:59 -0600

    Rename variables in benchmark/benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -18,19 +18,19 @@ end
 
 β = 40
 μ = 0.1
-𝐇 = setup_hamiltonian(100)
+𝐇 = setup_hamiltonian(1000)
 
 α = estimate_alpha(𝐇, μ)
 α_exact = compute_alpha(𝐇, μ)
 
 order = get_order(α, β)
-order_exact = get_order(α_exact, β)
+order_α_exact = get_order(α_exact, β)
 
 dm = density_matrix(𝐇, μ, α; order)
 N = tr(dm)
 
-dm_alpha = density_matrix(𝐇, μ, α_exact; order=order_exact)
-N_alpha = tr(dm_alpha)
+dm_α_exact = density_matrix(𝐇, μ, α_exact; order=order_α_exact)
+N_α_exact = tr(dm_α_exact)
 
 dm_exact = fermi_dirac(𝐇, μ, β)
 N_exact = tr(dm_exact)
