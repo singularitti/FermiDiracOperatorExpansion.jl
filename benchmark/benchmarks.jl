0af05472f7a1be02fe7122965feb915861f993bc
singularitti <singularitti@outlook.com>
2024-09-30 01:28:42 -0600

    Fix `setup_hamiltonian` in benchmark/benchmarks.jl

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -9,7 +9,7 @@ SUITE["rand"] = @benchmarkable rand(10)
 function setup_hamiltonian(N, a=0.01)
     𝐇 = diagm(10 * rand(N))
     foreach(1:size(𝐇, 1)) do i
-        foreach(1:size(𝐇, 2)) do j
+        foreach((i + 1):size(𝐇, 2)) do j
             𝐇[i, j] = exp(-a * (i - j)^2)  # Mimic a non-metallic system or a metallic system at ﬁnite temperature
         end
     end
