07effc91ab87762da2853c11e67b7e5180d65d2b
singularitti <singularitti@outlook.com>
2024-10-05 01:41:29 -0600

    Add `rescale_mu` in src/verification.jl

--- src/verification.jl
+++ src/verification.jl
@@ -1,9 +1,12 @@
 using Roots: Newton, find_zero
 
-export estimate_mu
+export estimate_mu, rescale_mu
 
 function estimate_mu(Nocc, 𝛆, β, μ₀; atol=1e-8, maxiters=50, verbose=true)
     g(μ) = Nocc - sum(fermi_dirac.(𝛆, μ, β))
     g′(μ) = sum(fermi_dirac_prime.(𝛆, μ, β))
     return find_zero((g, g′), μ₀, Newton(); atol=atol, maxiters=maxiters, verbose=verbose)
 end
+
+rescale_mu(μ′, α, μ) = (μ′ - oneunit(μ′) / 2) / α + μ
+# rescale_mu(μ′, α, μ) = α * (μ′ - μ) + oneunit(μ′) / 2
